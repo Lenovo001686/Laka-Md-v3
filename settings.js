1b4ad9d3a239a2a095b9c344d800b5cde1c55b21
Lenovo001686 <whatsappn584@gmail.com>
2025-06-06 21:50:29 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,6 +4,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Session id",
+SESSION_ID: process.env.SESSION_ID || "𝐋ᴀᴋᴀ-𝐌ᴅ=N5BBBBzR#8E8LgNP4CMNQZULNtVpK7ZEJrM88Jv1lLGD0j7LlAdo",
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:lakamd@db.ndduyrkebubwlbwiljee.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL
 };
